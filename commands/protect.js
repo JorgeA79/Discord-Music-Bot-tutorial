0b9739277aa62716802b33464866d332a19e878e
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 00:47:33 -0500

    Create protect.js

---
+++ commands/protect.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "protect",
+  description: "kiss someone owo",
+  execute(client, message, args) {
+      var hugifs =  [
+	 "https://i.gifer.com/3cA2.gif",
+ 	 "https://memestatic.fjcdn.com/gifs/Protect+the+cinnamon+roll_7619b3_6456881.gif"];
+
+		
+    
+	    let member = message.mentions.members.first();
+	    if(!member) 
+	    return message.reply("Try mentioning the person");	
+	
+     	
+	   var selecthugGif = hugifs[Math.floor(Math.random() * hugifs.length)];
+		
+	    const embed = new Discord.MessageEmbed()
+            .setDescription(`**${message.author.username}** protected **${member.user.username}**`)
+	    .setColor(0xC76CF5)
+            .setImage(selecthugGif);
+	    message.channel.send(embed);
+	   
+	   
+  }
+};
