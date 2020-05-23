e5d0d33446088c1247245af91b5f5b6edd3c6568
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 00:58:28 -0500

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,1 +1,18 @@
+const Discord = require("discord.js");
 
+module.exports = {
+  name: "hug",
+  description: "Hug someone owo",
+  execute(client, message, args) {
+    const args = message.content.slice(prefix.length).split(` `);
+		message.delete(1000);
+		if (!args.length) {
+		return message.channel.send(`You didn't say anything, ${message.author}!`);
+	  }
+		
+	const embed = new Discord.MessageEmbed()
+   .setColor(0xC76CF5)
+  .setDescription(args.splice(1).join(" "))
+  message.channel.send(embed);
+  }
+};
