4050cb461adcd978abd971cc2f55ca66caf7b1e6
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 02:48:02 -0500

    Update date.js

--- commands/date.js
+++ commands/date.js
@@ -7,9 +7,9 @@ module.exports = {
   let member = message.mentions.members.first();
 		 if(!member) 
 		return message.reply("Try mentioning the person");	
-		
-		  message.channel.send(`**${message.author.username}** dated **${member.user.username}**`);
+
 		const embed = new Discord.MessageEmbed()
+		 .setDescription(`**${message.author.username}** dated **${member.user.username}**`)
   		 .setColor(0xC76CF5)
   		.setImage("https://media1.tenor.com/images/e0f8ceace2d85bcdc36fe6d74f649b9f/tenor.gif");
    message.channel.send(embed);
