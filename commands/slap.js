ef5d5ad1280e4c444f0e25e835b59a7cc5f633c1
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 00:43:11 -0500

    Create slap.js

---
+++ commands/slap.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "slap",
+  description: "slap someone owo",
+  execute(client, message, args) {
+      var huggifs =  [
+	    "https://media1.tenor.com/images/b6d8a83eb652a30b95e87cf96a21e007/tenor.gif?itemid=10426943",
+ 	    "https://media.giphy.com/media/Zau0yrl17uzdK/giphy.gif",
+ 	    "https://i.pinimg.com/originals/4e/9e/a1/4e9ea150354ad3159339b202cbc6cad9.gif"];
+		
+    
+	    let member = message.mentions.members.first();
+	    if(!member) 
+	    return message.reply("Try mentioning the person");	
+	
+     	
+	   var selecthugGif = hugifs[Math.floor(Math.random() * hugifs.length)];
+		
+	    const embed = new Discord.MessageEmbed()
+            .setDescription(`**${message.author.username}** slapped **${member.user.username}**`)
+	    .setColor(0xC76CF5)
+            .setImage(selecthugGif);
+	    message.channel.send(embed);
+	   
+	   
+  }
+};
