d3eb5f4af5ea25e917d295b4ebc1ac7307a70d70
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 04:03:23 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
      	const embed = new Discord.MessageEmbed()
   .setTitle("Pixel's Commands")
    .setColor(0xC76CF5)
-  .setDescription("Here you can find the main commands of the bot so you wont get confused")
+  .setDescription("Here you can find the main commands of the bot so you wont get confused <a:?:713677852289269761>")
 .setThumbnail("https://cdn.discordapp.com/avatars/370483123848478721/2a073955469d1aefda2ce240ab5d2948.png?size=128")
 
   .addField("Main Commands",
