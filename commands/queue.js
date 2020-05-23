06ee656c4d68578d91a4fb4383af1f32a57a5fed
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 04:24:58 -0500

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 module.exports = {
   name: "queue",
   description: "get list of added songs",
@@ -5,20 +7,27 @@ module.exports = {
     const { channel } = message.member.voice;
     if (!channel) {
       //IF AUTHOR IS NOT IN VOICE CHANNEL
-      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/");
+          const embed = new Discord.MessageEmbed()
+          .setDescription("**You need to be in a voice channel** <a:x_:713677703756251147>")
+          .setColor(0xC76CF5);
+      return message.channel.send(embed);
     }
 
     const serverQueue = message.client.queue.get(message.guild.id);
 
     if (!serverQueue) {
-      return message.channel.send("There is nothing in the queue");
+        const embed = new Discord.MessageEmbed()
+          .setDescription("**There is nothing in the queue** <a:x_:713677703756251147>")
+          .setColor(0xC76CF5);
+        return message.channel.send(embed);
     }
-
-    message.channel.send(
-      `${serverQueue.songs
+      const embed = new Discord.MessageEmbed()
+          .setDescription(`${serverQueue.songs
         .map((song, index) => index + 1 + ". " + song.title)
         .join("\n\n")}`,
-      { split: true }
-    );
+      { split: true })
+          .setColor(0xC76CF5);
+         return message.channel.send(embed);
+
   }
 };
