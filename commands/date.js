74c785a9e12f233061f8b9c22905fd0e63f2a419
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 02:44:35 -0500

    Update date.js

--- commands/date.js
+++ commands/date.js
@@ -9,7 +9,7 @@ module.exports = {
 		return message.reply("Try mentioning the person");	
 		
 		  message.channel.send(`**${message.author.username}** dated **${member.user.username}**`);
-		const embed = new Discord.RichEmbed()
+		const embed = new Discord.MessageEmbed()
   		 .setColor(0xC76CF5)
   		.setImage("https://media1.tenor.com/images/e0f8ceace2d85bcdc36fe6d74f649b9f/tenor.gif");
    message.channel.send(embed);
