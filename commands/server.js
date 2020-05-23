973be988a5f0ca50d90f8419951cec7163b397fe
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 05:17:12 -0500

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -6,6 +6,7 @@ module.exports = {
   execute(client, message, args) {
       
 	 const embed = new Discord.MessageEmbed()
+	   .setTitle("> Click to join our server <")
   .setColor(0xC76CF5) 
   .setThumbnail("https://cdn.discordapp.com/avatars/370483123848478721/2a073955469d1aefda2ce240ab5d2948.png?size=128")
   .setTimestamp()
