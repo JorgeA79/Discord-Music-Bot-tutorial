439fa692e2355d5ca757aa8d5e6f84b2e6ecada4
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 03:01:15 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,7 +6,8 @@ module.exports = {
   description: "Pinging the bot",
   execute(client, message) {
     const embed = new Discord.MessageEmbed()
-    .setDescription(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``); 
+    .setDescription(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``)
+    .setColor(0xC76CF5);
   message.channel.send(embed);
     
 }
