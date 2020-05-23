d3da43f97747048c382f09be6146a605a6cca34c
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 01:35:02 -0500

    Create date.js

---
+++ commands/date.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "date",
+  description: "Date someone owo",
+  execute(client, message, args) {
+  let member = message.mentions.members.first();
+		 if(!member) 
+		return message.reply("Try mentioning the person");	
+		
+		  message.channel.send(`**${message.author.username}** dated **${member.user.username}**`);
+		const embed = new Discord.RichEmbed()
+   .setColor(0xC76CF5)
+  .setImage("https://media1.tenor.com/images/e0f8ceace2d85bcdc36fe6d74f649b9f/tenor.gif?itemid=13300869")
+   message.channel.send(embed);
+  }
+};
