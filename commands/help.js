24b4abcc618f998a38c9b6fefdbf1214d28ddf67
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 01:13:11 -0500

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "help",
+  description: "Help",
+  execute(client, message, args) {
+  
+     	const embed = new Discord.MessageEmbed()
+  .setTitle("Pixel's Commands")
+   .setColor(0xC76CF5)
+  .setDescription("Here you can find the main commands of the bot so you wont get confused")
+.setThumbnail("https://cdn.discordapp.com/avatars/370483123848478721/2a073955469d1aefda2ce240ab5d2948.png?size=128")
+
+  .addField("Main Commands",
+    "\`help\`,\`ping\`,\`server\`")
+
+  .addField("Fun Commands", "\`8ball\`, \`flip\` \`head\` or \`tail\`,\`say\`")
+.addField("Roleplay Commands", "\`hug\`,\`slap\`,\`kiss\`,\`shoot\`,\`protect\`,\`wave\`,\`date\`,\`dance\`,\`pat\`")
+.addField("Japan Commands", "\`profile\`,\`ammy\`,\`pixel\`,\`nia\`,\`jelly\`,\`aldrin\`,\`chuuni\`")
+	.addField("Music Commands", "\`play\`,\`leave\`")
+  message.channel.send(embed);
+  
+  }
+};
