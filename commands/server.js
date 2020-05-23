1b0911a5e888eff124a7c5173a38d67f6636d3ad
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 05:16:04 -0500

    Create server.js

---
+++ commands/server.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "server",
+  description: "kiss someone owo",
+  execute(client, message, args) {
+      
+	 const embed = new Discord.MessageEmbed()
+  .setColor(0xC76CF5) 
+  .setThumbnail("https://cdn.discordapp.com/avatars/370483123848478721/2a073955469d1aefda2ce240ab5d2948.png?size=128")
+  .setTimestamp()
+  .setURL("https://discord.gg/gwFXT55");
+  
+  message.channel.send(embed);
+	   
+  }
+};
