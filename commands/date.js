f835fea80ddb56cba2b1214432db4f97d5ffcb14
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 01:37:05 -0500

    Update date.js

--- commands/date.js
+++ commands/date.js
@@ -10,8 +10,8 @@ module.exports = {
 		
 		  message.channel.send(`**${message.author.username}** dated **${member.user.username}**`);
 		const embed = new Discord.RichEmbed()
-   .setColor(0xC76CF5)
-  .setImage("https://media1.tenor.com/images/e0f8ceace2d85bcdc36fe6d74f649b9f/tenor.gif?itemid=13300869")
+  		 .setColor(0xC76CF5)
+  		.setImage("https://media1.tenor.com/images/e0f8ceace2d85bcdc36fe6d74f649b9f/tenor.gif?itemid=13300869");
    message.channel.send(embed);
   }
 };
