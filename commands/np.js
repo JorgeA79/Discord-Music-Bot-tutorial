2821d2ec9febe953b4c29a96f906f24287cfe7e6
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 19:24:35 -0500

    Update np.js

--- commands/np.js
+++ commands/np.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 module.exports = {
   name: "np",
   description: "send the name of on going song",
@@ -5,14 +6,20 @@ module.exports = {
     
       const { channel } = message.member.voice;
     if (!channel) {
+       const embed = new Discord.MessageEmbed()
+    .setDescription("You need to be in a voice channel <a:x_:713677703756251147>")
+    .setColor(0xC76CF5);
       //IF AUTHOR IS NOT IN VOICE CHANNEL
-      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/");
+     return message.channel.send(embed);
     }
 
     const serverQueue = message.client.queue.get(message.guild.id);
 
     if (!serverQueue) {
-      return message.channel.send("Bot is not playing anything");
+     const embed = new Discord.MessageEmbed()
+      .setDescription("Bot is not playing anything <a:x_:713677703756251147>")
+      .setColor(0xC76CF5);
+      return message.channel.send(embed);
     }
     
     message.channel.send(serverQueue.songs[0].title)
