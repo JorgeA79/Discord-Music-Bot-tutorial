04ad6f173a60cdbb7be8f786c6475c263debe279
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-23 19:14:20 -0500

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 module.exports = {
   name: "skip",
   description: "Skip the song or shift song to next",
@@ -5,17 +7,26 @@ module.exports = {
     const { channel } = message.member.voice;
 
     if (!channel) {
+          const embed = new Discord.MessageEmbed()
+    .setDescription("You need to be in a voice channel <a:x_:713677703756251147>")
+    .setColor(0xC76CF5);
       //IF AUTHOR IS NOT IN VOICE CHANNEL
-      return message.channel.send("YOU NEED TO BE IN VOICE CHANNEL :/");
+     return message.channel.send(embed);
     }
 
     const serverQueue = message.client.queue.get(message.guild.id);
 
     if (!serverQueue) {
-      return message.channel.send("There is nothing playing that i could skip");
+      const embed = new Discord.MessageEmbed()
+      .setDescription("There is nothing playing that i could skip <a:x_:713677703756251147>")
+      .setColor(0xC76CF5);
+      return message.channel.send(embed);
     }
 
     serverQueue.connection.dispatcher.end();
-    message.channel.send("✔ | Skipping The Song");
+    const embed = new Discord.MessageEmbed()
+    .setDescription("✔ | Skipping The Song")
+    .setColor(0xC76CF5);
+    serverQueue.textChannel.send(embed);
   }
 };
