56c487a9c63430da2aaf7f51b2ac87bda0fc8496
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-24 00:09:33 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -16,7 +16,6 @@ module.exports = {
 
   .addField("Fun Commands", "\`8ball\`, \`flip\` \`head\` or \`tail\`,\`say\`")
 .addField("Roleplay Commands", "\`hug\`,\`slap\`,\`kiss\`,\`shoot\`,\`protect\`,\`wave\`,\`date\`,\`dance\`,\`pat\`")
-.addField("Japan Commands", "\`profile\`,\`ammy\`,\`pixel\`,\`nia\`,\`jelly\`,\`aldrin\`,\`chuuni\`")
 	.addField("Music Commands", "\`play\`,\`stop\`,\`np\`,\`queue\`,\`pause\`,\`resume\`,\`lyrics\`,\`skip\`,\`clear\`")
   message.channel.send(embed);
   
