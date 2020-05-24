fbd81ec027d2a5d8708c34475396ad8bb339913b
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-24 00:05:55 -0500

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "say",
+  description: "Dance with someone owo",
+  execute(client, message, args) {
+
+   		message.channel.send(args);
+  }
+};
