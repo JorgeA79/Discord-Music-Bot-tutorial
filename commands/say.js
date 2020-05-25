06d71c27ef61adc1565f3188f3e85f822771de60
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-24 22:46:29 -0500

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -4,6 +4,6 @@ module.exports = {
   name: "say",
   description: "Dance with someone owo",
   execute(client, message, args) 
-   		message.channel.send(args.join(" "));
+   		message.channel.send(args);
   }
 };
