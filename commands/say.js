2631427fc4de798fcebdff5d845079d1f0279799
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-24 22:47:40 -0500

    Delete say.js

--- commands/say.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports = {
-  name: "say",
-  description: "Dance with someone owo",
-  execute(client, message, args) 
-   		message.channel.send(args);
-  }
-};
