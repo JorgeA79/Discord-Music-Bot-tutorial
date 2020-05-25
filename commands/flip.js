49e7323f79c00f584b01d4daad97a738f91d2b26
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-24 23:58:19 -0500

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -9,13 +9,13 @@ module.exports = {
     var result = Math.floor((Math.random() * 2) + 1);
     	      if (result == 1) {
             const embed = new Discord.MessageEmbed()
-            .setDescription(`WOOOOOOOOOOO <:wholesomekermit:705354033799364628> `)
+            .setDescription(`I wooooon uwu <:wholesomekermit:705354033799364628> `)
             .setTitle("Head")
             .setColor(0xC76CF5)
             .setImage("https://68.media.tumblr.com/4c0e4d4f186433f84ad11109f0b619b2/tumblr_np6oolnI2c1td4t64o1_500.gif");
-            message.channel.send({embed});
+            message.channel.send(embed);
             } else if (result == 2) {
-    		    const embed = new Discord.RichEmbed()
+    		    const embed = new Discord.MessageEmbed()
             .setTitle("Tail")
             .setDescription("`You got me <:wholesomekermit:705354033799364628> `")
             .setColor(0xC76CF5)
@@ -24,7 +24,22 @@ module.exports = {
     	      }	
     
     }else if(args[0] == "head"){
-    message.channel.send("puede ser");
+        var result = Math.floor((Math.random() * 2) + 1);
+    	      if (result == 1) {
+            const embed = new Discord.MessageEmbed()
+            .setDescription(`I wooooon uwu <:wholesomekermit:705354033799364628> `)
+            .setTitle("Head")
+            .setColor(0xC76CF5)
+            .setImage("https://68.media.tumblr.com/4c0e4d4f186433f84ad11109f0b619b2/tumblr_np6oolnI2c1td4t64o1_500.gif");
+            message.channel.send(embed);
+            } else if (result == 2) {
+    		    const embed = new Discord.MessageEmbed()
+            .setTitle("Tail")
+            .setDescription("`You got me <:wholesomekermit:705354033799364628> `")
+            .setColor(0xC76CF5)
+            .setImage("https://68.media.tumblr.com/4c0e4d4f186433f84ad11109f0b619b2/tumblr_np6oolnI2c1td4t64o1_500.gif");
+            message.channel.send(embed);
+    	      }	 
     }else{
      message.channel.send("no");
     
