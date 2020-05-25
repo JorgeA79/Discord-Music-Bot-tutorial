f7788d22ff90f0dd8fabfd9290882ce075472825
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-24 22:45:08 -0500

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -3,8 +3,7 @@ const Discord = require("discord.js");
 module.exports = {
   name: "say",
   description: "Dance with someone owo",
-  execute(client, message, args) {
-  const argso = message.content.slice(prefix.length).split(` `);
-   		message.channel.send(args.splice(1).join(" "));
+  execute(client, message, args) 
+   		message.channel.send(args.join(" "));
   }
 };
