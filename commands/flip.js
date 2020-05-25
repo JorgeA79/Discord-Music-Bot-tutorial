fe0c79febe39bba24a019a69b55a7f66dfeaa5da
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-24 23:55:52 -0500

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -6,7 +6,23 @@ module.exports = {
   execute(client, message, args) {
   
   if(args[0] == "tail"){
-  message.channel.send("yes");
+    var result = Math.floor((Math.random() * 2) + 1);
+    	      if (result == 1) {
+            const embed = new Discord.MessageEmbed()
+            .setDescription(`WOOOOOOOOOOO <:wholesomekermit:705354033799364628> `)
+            .setTitle("Head")
+            .setColor(0xC76CF5)
+            .setImage("https://68.media.tumblr.com/4c0e4d4f186433f84ad11109f0b619b2/tumblr_np6oolnI2c1td4t64o1_500.gif");
+            message.channel.send({embed});
+            } else if (result == 2) {
+    		    const embed = new Discord.RichEmbed()
+            .setTitle("Tail")
+            .setDescription("`You got me <:wholesomekermit:705354033799364628> `")
+            .setColor(0xC76CF5)
+            .setImage("https://68.media.tumblr.com/4c0e4d4f186433f84ad11109f0b619b2/tumblr_np6oolnI2c1td4t64o1_500.gif");
+            message.channel.send(embed);
+    	      }	
+    
     }else if(args[0] == "head"){
     message.channel.send("puede ser");
     }else{
