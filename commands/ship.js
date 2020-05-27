963088a12f4ca7a511805c0c9521470b0b1b8114
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-27 15:07:16 -0500

    Update ship.js

--- commands/ship.js
+++ commands/ship.js
@@ -59,12 +59,34 @@ module.exports = {
 	.setColor(0xC76CF5)
 	message.channel.send(embed);
 		
-	}else{	
+	}else if(message.author.id =="206606985167110145" && mention1.id == "438185201844224001"){
+		
+	const embed = new Discord.MessageEmbed()
+        .setDescription(`**${message.author.username}** has a Perfect match with **${mention1.username}** \n**Perfect** :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: `)
+	.setColor(0xC76CF5)
+	message.channel.send(embed);
+		
+	}else if(message.author.id =="438185201844224001" && mention1.id == "206606985167110145"){
+		
+	const embed = new Discord.MessageEmbed()
+        .setDescription(`**${message.author.username}** has a Perfect match with **${mention1.username}** \n**Perfect** :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: `)
+	.setColor(0xC76CF5)
+	message.channel.send(embed);
+		
+	}
+		
+		
+		
+		
+		
+	else{	
 	const embed = new Discord.MessageEmbed()
         .setDescription(`**${message.author.username}** matches ${percentage}% with **${mention1.username}** \n**${percentage}%** `+ desc)
 	.setColor(0xC76CF5)
 	message.channel.send(embed);
 	}
+		
+		
 	}else if(mention1.id == "304357538101723137" && mention2.id == "360207757787398145"){
 	const embed = new Discord.MessageEmbed()
         .setDescription(`**${mention1.username}** has a Perfect match with **${mention2.username}** \n**Perfect** :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: :green_square: `)
