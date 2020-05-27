c0a960d14cce0c0d8864fb32f9a238e9327ce5d2
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-27 03:56:40 -0500

    Update ship.js

--- commands/ship.js
+++ commands/ship.js
@@ -9,6 +9,33 @@ module.exports = {
   	mention1 = mention.first();
 	mention2 = mention.last();
 	console.log(mention2);
+	var percentage = Math.floor(Math.random() * 100) + 1; 
+	const desc = "";  
+	
+	if(percentage > 0 && percentage <10) {
+	desc = ":black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	else if(percentage > 10 && percentage <20) {
+	desc = ":white_medium_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	}else if(percentage > 20 && percentage <30) {	
+	desc = ":white_medium_square: :white_medium_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	}else if(percentage > 30 && percentage <40) {
+	desc = ":white_medium_square: :white_medium_square: :white_medium_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	}else if(percentage > 40 && percentage <50) {
+	desc = ":white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	}else if(percentage > 50 && percentage <60) {
+	desc = ":white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	}else if(percentage > 60 && percentage <70) {
+	desc = ":white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square:"
+	}else if(percentage > 70 && percentage <80) {
+	desc = ":white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :black_large_square: :black_large_square: :black_large_square:"
+	}else if(percentage > 80 && percentage <90) {
+	desc = ":white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :black_large_square: :black_large_square:"
+	}else if(percentage > 90 && percentage <100) {
+	desc = ":white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :black_large_square:"
+	}else if(percentage == 100) {
+	desc = ":white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: :white_medium_square: white_medium_square:"	
+	}
+	  
 	  
 	if(mention1 == mention2){
 	mention2 = "";
@@ -18,7 +45,7 @@ module.exports = {
 	return message.reply(`Try mentioning the person`);	
 	}else if(!mention2){
 	const embed = new Discord.MessageEmbed()
-        .setDescription(`**${message.author.username}** matches % with **${mention1.username}** \n:black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: :black_large_square: `)
+        .setDescription(`**${message.author.username}** matches % with **${mention1.username}** \n**${percentage}%** ${desc} `)
 	.setColor(0xC76CF5)
 	message.channel.send(embed);
 	
