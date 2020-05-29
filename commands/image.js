31349741b10247407368d7a9b860a3734e93bdfc
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-28 21:32:17 -0500

    Update image.js

--- commands/image.js
+++ commands/image.js
@@ -1,12 +1,42 @@
 //FIRST TEST HANDLER IS WORKING OR NOT
 const Discord = require("discord.js");
+const Canvas = require('canvas');
 
 module.exports = {
   name: "image",
   description: "Pinging the bot",
   execute(client, message) {
 
+const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
 
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Owo,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${message.author.username}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${message.author.username}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`OwO, ${message.author.username}!`, attachment);
 
 
     
