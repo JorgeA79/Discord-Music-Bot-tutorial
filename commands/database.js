40b07beb3e21de3253892357ce4b6256eacb350d
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-28 20:52:19 -0500

    Update database.js

--- commands/database.js
+++ commands/database.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js");
 const pg = require('pg');
 
-    const pool = new pg.Pool({
-	      port: 5432,
-        	host: process.env.dbhost,
-        	database: process.env.db,
-        	user: process.env.user,
-        	password: process.env.password,
-        });
-      pool.connect()
-    
+const pool = new pg.Pool({
+	port: 5432,
+        host: 'ec2-54-211-210-149.compute-1.amazonaws.com',
+        database: 'dbi7a4i58mse1i',
+        user: 'yuoiefmjewbrjt',
+        password: '9bea5f1fa4e2b903889c29e14b98e19e7d5e6612980a22e84b56d881c2c1bf74',
+});
+
+pool.connect()
+
 //pool.query("CREATE TABLE userxp (id INT, xp INT, lvl INT)", (err, res)=>{
 // console.log(err,res)
 //  pool.end()
