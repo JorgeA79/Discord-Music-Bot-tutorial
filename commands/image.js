8adcbb61087b49c053c6ffa011a606931773e6b7
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-28 21:25:11 -0500

    Create image.js

---
+++ commands/image.js
@@ -0,0 +1,14 @@
+//FIRST TEST HANDLER IS WORKING OR NOT
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "image",
+  description: "Pinging the bot",
+  execute(client, message) {
+
+
+
+
+    
+}
+}
