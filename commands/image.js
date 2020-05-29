7974b6e608dadc1d8f9f0577891545e5bd58be2c
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-28 21:33:49 -0500

    Update image.js

--- commands/image.js
+++ commands/image.js
@@ -10,7 +10,7 @@ module.exports = {
 const canvas = Canvas.createCanvas(700, 250);
 	const ctx = canvas.getContext('2d');
 
-	const background = await Canvas.loadImage('./wallpaper.jpg');
+	const background = await Canvas.loadImage('./2vsIPEP.3f295fd2.png');
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
 	ctx.strokeStyle = '#74037b';
