b42a17f6bae1edae10b06a5a3aa134ef09efa52a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-28 20:28:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -211,48 +211,43 @@ const pg = require('pg')
 const pool = new pg.Pool({
 	connectionString : process.env.DATABASE_URL,
 	port: 5432,
-        host: process.env.dbhost,
-        database: process.env.db,
-        user: process.env.user,
-        password: process.env.password,
+        host: 'ec2-54-211-210-149.compute-1.amazonaws.com',
+        database: 'dbi7a4i58mse1i',
+        user: 'yuoiefmjewbrjt',
+        password: '9bea5f1fa4e2b903889c29e14b98e19e7d5e6612980a22e84b56d881c2c1bf74',
         ssl: true,
 })
 pool.connect()
 
 
-//client.on('message', message => {
-//	  if (message.author === client.user) return;
-//	if(message.channel.type === "dm") return;
-//	 if (talkedRecently.has(message.author.id)) {
-            
- //   		} else {
-
-           // the user can type the command ... your command code goes here :)
-
-        // Adds the user to the set so that they can't talk for a minute
+client.on('message', message => {
+	 if (message.author === client.user) return;
+ 	 if(message.channel.type === "dm") return;
+	 if (talkedRecently.has(message.author.id)) {
+           
+ } else {
     
-//		pool.query(`SELECT * FROM usersxp WHERE id = '${message.author.id}'`, (err,result) =>{
-   //		 if(err) throw err;
-   // 		let sql;  
- //   		if(!result.rows[0]){
- //   		sql = `INSERT INTO usersxp (id,xp) VALUES ('${message.author.id}', ${generateXp()})`;
-  //  		} else {
-  //   		let xp = result.rows[0].xp;
-  //   		sql = `UPDATE usersxp SET xp = ${xp + generateXp()} WHERE id = '${message.author.id}'`;
+		pool.query(`SELECT * FROM usersxp WHERE id = '${message.author.id}'`, (err,result) =>{
+      		if(err) throw err;
+   		let sql;  
+    		if(!result.rows[0]){
+    		sql = `INSERT INTO usersxp (id,xp) VALUES ('${message.author.id}', ${generateXp()})`;
+    		} else {
+     		let xp = result.rows[0].xp;
+     		sql = `UPDATE usersxp SET xp = ${xp + generateXp()} WHERE id = '${message.author.id}'`;
           
-    //		}  
- //    		pool.query(sql, console.log);
-   // 		});
+    		}  
+    		pool.query(sql, console.log);
+    		});
 	    
-//	        talkedRecently.add(message.author.id);
-//	    
-   //     setTimeout(() => {
-          // Removes the user from the set after a minute
-  //        talkedRecently.delete(message.author.id);
-    //    }, 60000);
- //   }
-	
-//});
+	        talkedRecently.add(message.author.id);
+	    
+   		setTimeout(() => {
+ 
+ 		talkedRecently.delete(message.author.id);
+		}, 60000);
+		}
+		});
 
 function generateXp(){
 let min = 10;
