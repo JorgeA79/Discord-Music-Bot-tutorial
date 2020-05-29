41050fe784d8ed4922b0d803a9487a4d4b9e55d7
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-28 21:15:02 -0500

    Update database.js

--- commands/database.js
+++ commands/database.js
@@ -2,11 +2,7 @@ const Discord = require("discord.js");
 const pg = require('pg');
 
 const pool = new pg.Pool({
-	port: 5432,
-        host: 'ec2-54-211-210-149.compute-1.amazonaws.com',
-        database: 'dbi7a4i58mse1i',
-        user: 'yuoiefmjewbrjt',
-        password: '9bea5f1fa4e2b903889c29e14b98e19e7d5e6612980a22e84b56d881c2c1bf74',
+connectionString : process.env.DATABASE_URL,
 });
 
 pool.connect()
