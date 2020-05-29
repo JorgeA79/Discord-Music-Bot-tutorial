23f6e89f248b8566c28113adb659def51a023a44
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-28 20:40:29 -0500

    Update database.js

--- commands/database.js
+++ commands/database.js
@@ -1,19 +1,6 @@
 const Discord = require("discord.js");
-const pg = require('pg')
+const pg = require('pg');
 
-
-//pool.query("CREATE TABLE userxp (id INT, xp INT, lvl INT)", (err, res)=>{
-// console.log(err,res)
-//  pool.end()
-// })
-
-module.exports = {
-  name: "xp",
-  description: "Pinging the bot",
-  execute(client, message) {
-
-//pool.query('INSERT INTO userxp (id, xp, lvl) VALUES ($1, $2, $3)', [idx, xd, dx])
- 
     const pool = new pg.Pool({
 	    connectionString : process.env.DATABASE_URL,
 	      port: 5432,
@@ -25,12 +12,22 @@ module.exports = {
         })
       pool.connect()
     
-    
+//pool.query("CREATE TABLE userxp (id INT, xp INT, lvl INT)", (err, res)=>{
+// console.log(err,res)
+//  pool.end()
+// })
 
-   let target = message.mentions.users.first() || message.author;
+module.exports = {
+  name: "xp",
+  description: "Pinging the bot",
+  execute(client, message) {
+
+//pool.query('INSERT INTO userxp (id, xp, lvl) VALUES ($1, $2, $3)', [idx, xd, dx])
+
+    let target = message.mentions.users.first() || message.author;
     
     pool.query(`SELECT * FROM usersxp WHERE id = '${target.id}'`,(err, result)=>{
-    if(err) return err;
+    if(err) throw err;
     if(!result.rows[0])  return message.channel.send("This user has no xp")
       
     let xp = result.rows[0].xp;
