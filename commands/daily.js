f78f4a390ca80be35960784c665b7fe00f05fc77
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-31 03:44:38 -0500

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -14,22 +14,28 @@ module.exports = {
 	  
 	  
 pool.query(`SELECT * FROM usersxp WHERE id = '${message.author.id}'`, (err,result) =>{
-   if(err) return err;
-	  
+   if(err) return err;	  
   let cooldown = 8.64e+7,
   amount = 250;
-  let money;
+ 
 	
   let lastDaily = result.rows[0].lastD;	  
+	
   if(lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0){
+	  
   let timeObj = ms(cooldown -(Date.now() - lastDaily));
   
   message.channel.send(`Your already collected, please wait **${timeObj.hours}h ${timeObj.minutes}m**!`)
+  
   } else {
+	  
   message.send(`Succesfully collected $${amount}`);
   
   pool.query(`UPDATE usersxp SET lastD = ${Date.now()} WHERE id = '${message.author.id}'`, console.log);	  
-  if(result.rows[0].money === null) money=0;	  
+  
+   let money = result.rows[0].money;
+   if(money === null) money=0;
+  	  
   pool.query(`UPDATE usersxp SET money = ${money + amount} WHERE id = '${message.author.id}'`, console.log);		  
   
   
