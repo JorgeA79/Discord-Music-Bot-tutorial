b76b46f51b5a6f2cc22428eff1fcefcbfbe3aa42
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-31 04:58:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -281,10 +281,7 @@ client.on('message', async message => {
 	ctx.font = '28px sans-serif';
 	ctx.fillStyle = '#ffffff';
 	ctx.fillText(`Your XP: ${xp}`, canvas.width / 2.5, canvas.height / 1.8);
-	
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`Your money: $${money}`, canvas.width / 2.5, canvas.height / 0.1);	
+	ctx.fillText(`Your money: $${money}`, canvas.width / 2.5, canvas.height / 1.0);	
 
 	ctx.font = applyText(canvas, `${target.username}!`);
 	ctx.fillStyle = '#ffffff';
