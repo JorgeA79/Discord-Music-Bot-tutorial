1fad92e8027d01beda378e4f5593c03be152800a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-31 03:40:31 -0500

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -10,10 +10,32 @@ module.exports = {
   name: "daily",
   description: "Pinging the bot",
   execute(client, message) {
-  console.log(Date.now());
   
-  let sql = `ALTER TABLE usersxp ADD COLUMN lastD BIGINT`;
-  pool.query(sql, console.log);
-    
+	  
+	  
+pool.query(`SELECT * FROM usersxp WHERE id = '${message.author.id}'`, (err,result) =>{
+   if(err) return err;
+	  
+  let cooldown = 8.64e+7,
+  amount = 250;
+  let money;
+	
+  let lastDaily = result.rows[0].lastD;	  
+  if(lastDaily !== null && cooldown - (Date.now() - lastDaily) > 0){
+  let timeObj = ms(cooldown -(Date.now() - lastDaily));
+  
+  message.channel.send(`Your already collected, please wait **${timeObj.hours}h ${timeObj.minutes}m**!`)
+  } else {
+  message.send(`Succesfully collected $${amount}`);
+  
+  pool.query(`UPDATE usersxp SET lastD = ${Date.now()} WHERE id = '${message.author.id}'`, console.log);	  
+  if(result.rows[0].money === null) money=0;	  
+  pool.query(`UPDATE usersxp SET money = ${money + amount} WHERE id = '${message.author.id}'`, console.log);		  
+  
+  
+        }		
+	)};  
+	   
+	   
 }
 }
