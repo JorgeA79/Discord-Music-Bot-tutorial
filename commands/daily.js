8ec79f7d19187d14191e7c1228f471dbab86a80a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-31 04:16:47 -0500

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,6 +1,7 @@
 const db = require('quick.db');
 const Discord = require("discord.js");
 const pg = require('pg')
+const ms = require('parse-ms')
 const pool = new pg.Pool({
 	connectionString : process.env.DATABASE_URL,
 });
