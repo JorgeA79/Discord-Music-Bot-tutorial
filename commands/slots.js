1f6bca28fbc938381ff73da5530562e3fac3be62
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-01 03:56:43 -0500

    Update slots.js

--- commands/slots.js
+++ commands/slots.js
@@ -6,33 +6,55 @@ module.exports = {
   description: "Pinging the bot",
   execute(client, message) {
 
-const cherry = new SlotSymbol('cherry', {
-    display: '🍒',
+const pokeb = new SlotSymbol('pokeb', {
+    display: '<:pokeb:716936621265518613>',
     points: 10,
     weight: 100
 });
- 
-const money = new SlotSymbol('money', {
-    display: '💰',
+const greatb = new SlotSymbol('greatb', {
+    display: '<:greatb:716936621022248970>',
+    points: 10,
+    weight: 100
+});    
+const ultrab = new SlotSymbol('ultrab', {
+    display: '<:ultrab:716936621529890869>',
+    points: 10,
+    weight: 100
+});  
+const premierb = new SlotSymbol('premierb', {
+    display: '<:premierb:716936621592674365>',
+    points: 10,
+    weight: 100
+});
+const quickb = new SlotSymbol('quickb', {
+    display: '<:quickb:716936621609582715>',
+    points: 10,
+    weight: 100
+});
+const timerb = new SlotSymbol('timerb', {
+    display: '<:timerb:716936621680885760>',
+    points: 10,
+    weight: 100
+});    
+const loveb = new SlotSymbol('loveb', {
+    display: '<:loveb:716936621118849034>',
+    points: 10,
+    weight: 100
+});  
+const luxuryb = new SlotSymbol('luxuryb', {
+    display: '<:luxuryb:716936620753813586>',
+    points: 10,
+    weight: 100
+});      
+const masterb = new SlotSymbol('masterb', {
+    display: '<:masterb:716936718825029723>',
     points: 100,
     weight: 50
 });
  
-const wild = new SlotSymbol('wild', {
-    display: '❔',
-    points: 10,
-    weight: 50,
-    wildcard: true
-});
-    
-const xd = new SlotSymbol('xd', {
-    display: '<:wait:694792686783168524>',
-    points: 30,
-    weight: 100,
-    wildcard: true
-});    
+
  
-const machine = new SlotMachine(3, [cherry, money, wild, xd]);
+const machine = new SlotMachine(3, [pokeb, greatb, ultrab, quickb, luxuryb, timerb, loveb, premierb, masterb]);
 const results = machine.play();
 
 message.channel.send(results.visualize());    
