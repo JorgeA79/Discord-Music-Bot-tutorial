06c724f7bb6d65210710258d875ce5a8ddf946db
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-01 19:47:02 -0500

    Create osu.js

---
+++ commands/osu.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "osu",
+  description: "Pinging the bot",
+  execute(client, message, args) {
+  
+  if(!args[0]){
+          const embed = new Discord.MessageEmbed()
+            .setDescription(`You need to specify an osu mode`)
+            .setColor(0xC76CF5)
+            return message.channel.send(embed);
+  }
+  if(!args[1]){
+   const embed = new Discord.MessageEmbed()
+            .setDescription(`You need to specify an user`)
+            .setColor(0xC76CF5)
+            return message.channel.send(embed);
+ }
+  if(args[0] == "taiko"){
+    getOsu(`1`,args[1]);
+    }else if(args[0] == "osu"){
+      getOsu(`0`,args[1]);
+    }
+    else if(args[0] == "ctb"){
+      getOsu(`2`,args[1]);
+    }
+    else if(args[0] == "mania"){ 
+      getOsu(`3`,args[1]);
+    }
+}
+}
+
+
+function getOsu(mode, name){
+          const embed = new Discord.MessageEmbed()
+            .setDescription(`${name}'s Signature`)
+            .setImage(`https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname=${name}&mode=${mode}&countryrank&flagshadow&flagstroke&darktriangles&opaqueavatar&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
+            .setColor(0xC76CF5)
+            message.channel.send(embed);
+}
