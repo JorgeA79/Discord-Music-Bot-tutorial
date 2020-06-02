6281848ce544ce49399ab7c906ea2260239e4c79
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-01 22:04:50 -0500

    Create args.js

---
+++ commands/args.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'args',
+	description: 'Hugging.',
+	cooldown: 5,
+	execute(message) {
+  
+		const args = message.content.slice(PREFIX.length).split(` `);
+    if (!args.length) {
+		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+	   }
+    
+    message.channel.send(args.splice(1));
+    
+	}
+};
