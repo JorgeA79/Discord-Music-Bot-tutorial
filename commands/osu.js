36e433028f98287d4af467dd2b9acd77ef8d18d9
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-01 21:17:37 -0500

    Update osu.js

--- commands/osu.js
+++ commands/osu.js
@@ -17,10 +17,10 @@ module.exports = {
             .setColor(0xC76CF5)
             return message.channel.send(embed);
  }
- let args2= args[2];
- let args3= args[3];
- let args4= args[4];  
- let args5= args[5];  
+ let args2= ` args[2]`;
+ let args3= ` args[3]`;
+ let args4= ` args[4]`;  
+ let args5= ` args[5]`;  
  if(!args[2]) args2 = "";
  if(!args[3]) args3 = "";
  if(!args[4]) args4 = "";
@@ -29,31 +29,31 @@ module.exports = {
   if(args[0] == "taiko"){
 
     const embed = new Discord.MessageEmbed()
-            .setDescription(`${args[1]} ${args2} ${args3} ${args4} ${args5}'s Signature`)
-            .setImage(`https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname=${args[1]} ${args2} ${args3} ${args4} ${args5}&mode=1&countryrank&flagshadow&flagstroke&darktriangles&opaqueavatar&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
+            .setDescription(`${args[1]}${args2}${args3}${args4}${args5}'s Signature`)
+            .setImage(`https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname=${args[1]}${args2}${args3}${args4}${args5}&mode=1&countryrank&flagshadow&flagstroke&darktriangles&opaqueavatar&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
             .setColor(0xC76CF5);
      message.channel.send(embed);
     
   }else if(args[0] == "osu"){
 
      const embed = new Discord.MessageEmbed()
-            .setDescription(`${args[1]} ${args2} ${args3} ${args4} ${args5}'s Signature`)
-            .setImage(`https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname=${args[1]} ${args2} ${args3} ${args4} ${args5}&mode=0&countryrank&flagshadow&flagstroke&darktriangles&opaqueavatar&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
+            .setDescription(`${args[1]}${args2}${args3}${args4}${args5}'s Signature`)
+            .setImage(`https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname=${args[1]}${args2}${args3}${args4}${args5}&mode=0&countryrank&flagshadow&flagstroke&darktriangles&opaqueavatar&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
             .setColor(0xC76CF5);
             message.channel.send(embed);
     }
     else if(args[0] == "ctb"){
       const embed = new Discord.MessageEmbed()
-            .setDescription(`${args[1]} ${args2} ${args3} ${args4} ${args5}'s Signature`)
-            .setImage(`https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname=${args[1]} ${args2} ${args3} ${args4} ${args5}&mode=2&countryrank&flagshadow&flagstroke&darktriangles&opaqueavatar&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
+            .setDescription(`${args[1]}${args2}${args3}${args4}${args5}'s Signature`)
+            .setImage(`https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname=${args[1]}${args2}${args3}${args4}${args5}&mode=2&countryrank&flagshadow&flagstroke&darktriangles&opaqueavatar&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
             .setColor(0xC76CF5);
             message.channel.send(embed);
     }
     else if(args[0] == "mania"){ 
       getOsu(`3`,args[1]);
      const embed = new Discord.MessageEmbed()
-            .setDescription(`${args[1]} ${args2} ${args3} ${args4} ${args5}'s Signature`)
-            .setImage(`https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname=${args[1]} ${args2} ${args3} ${args4} ${args5}&mode=3&countryrank&flagshadow&flagstroke&darktriangles&opaqueavatar&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
+            .setDescription(`${args[1]}${args2}${args3}${args4}${args5}'s Signature`)
+            .setImage(`https://lemmmy.pw/osusig/sig.php?colour=hexff66aa&uname=${args[1]}${args2}${args3}${args4}${args5}&mode=3&countryrank&flagshadow&flagstroke&darktriangles&opaqueavatar&rankedscore&onlineindicator=undefined&xpbar&xpbarhex`)
             .setColor(0xC76CF5);
             message.channel.send(embed);
     }
