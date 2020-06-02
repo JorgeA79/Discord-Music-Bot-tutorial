499cc991e432e6e68a38912f4f41b2f22b946e33
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-02 16:08:25 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,14 +11,14 @@ module.exports = {
   .setDescription("Here you can find the main commands of the bot so you wont get confused <a:__:713677852289269761>")
 .setThumbnail(process.env.BOT_AVATAR)
 
-  .addField("Main Commands",
+  .addField(":shield: Main Commands",
     "\`help\` \`ping\` \`server\` \`balance\` \`daily\` \`profile\`", true)
 
-  .addField("Fun Commands", "\`8ball\` \`flip\` \`head\` or \`tail\` \`say\` \`ship\` \`slots\`", true)
-.addField("Roleplay Commands", "\`hug\` \`slap\` \`kiss\` \`shoot\`,\`protect\` \`wave\` \`date\` \`dance\` \`pat\`", true)
-.addField("Music Commands", "\`play\` \`stop\` \`np\` \`queue\` \`pause\` \`resume\` \`lyrics\` \`skip\` \`clear\`", true)
-.addField("Anime Commands", "\`pokedex\`",true)
-.addField("Game Commands", "\`osu\`",true)      
+  .addField(":8ball: Fun Commands", "\`8ball\` \`flip\` \`head\` or \`tail\` \`say\` \`ship\` \`slots\`", true)
+.addField("<:kannaheart:712714267056537641> Roleplay Commands", "\`hug\` \`slap\` \`kiss\` \`shoot\`,\`protect\` \`wave\` \`date\` \`dance\` \`pat\`", true)
+.addField(":musical_note: Music Commands", "\`play\` \`stop\` \`np\` \`queue\` \`pause\` \`resume\` \`lyrics\` \`skip\` \`clear\`", true)
+.addField("<:pokeb:716936621265518613> Anime Commands", "\`pokedex\`",true)
+.addField(":video_game: Game Commands", "\`osu\`",true)      
   message.channel.send(embed);
   
   }
