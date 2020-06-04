93cb13ac4555f31087ab50186eedeb685f37e96c
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-04 11:47:04 -0500

    Update marry.js

--- commands/marry.js
+++ commands/marry.js
@@ -12,35 +12,56 @@ module.exports = {
   description: "Pinging the bot",
   execute(client, message) {
  
+	  pool.query(`SELECT * FROM usersxp WHERE id = '${message.author.id}'`, (err,result) =>{
+	  
+	  
 	   let member = message.mentions.members.first();
 	   if(!member) return message.reply("Try mentioning the person");
 	   if(member.user.id == message.author.id) return message.reply("You cannot marry with yourself");
 	  
- 	  message.channel.send(`${member.user}, Do you want to marry with ${message.author}`)
+	  	  
+		  
+	  let marry = result.rows[0].marry;
+	  if(marry === null){  
+ 	  pool.query(`SELECT * FROM usersxp WHERE id = '${member.user.id}'`, (err,resultx) =>{
+          let marryxd = resultx.rows[0].marryxd;
+	  if(marry === null){  
+		  
+	  }else{
+	  return message.reply("Is already married");
+	  }	  
+	  });		  
+		  
+		  
+	  message.channel.send(`${member.user}, Do you want to marry with ${message.author}`)
 	  const collector = new Discord.MessageCollector(message.channel, m => m.author.id === member.user.id, { time: 10000 });
           console.log(collector)
           collector.on('collect', m => {
           if (m.content == "yes") {
-	
-		 pool.query(`ALTER TABLE usersxp ADD COLUMN marry BIGINT`, console.log);	  
 		  
-                m.channel.send("Accepted");
+	  	
+			
+		m.channel.send("Accepted");
 	  	collector.stop('Collector stopped manually');
-		  
+	
+			   
           } else if (m.content == "no") {
 	
-                
-		  
-		  
+  
 		m.channel.send("Denied");
 		collector.stop('Collector stopped manually'); 
-          }
-    
+          }  
 	  })
 	  collector.on('end', collected => {
 	  if(collected.size === 0){
 	  return message.reply("No one replied");
 	  }		  
+	  });
+	   }else {
+	   
+	   return message.reply("You are already married");
+	   
+	   }
 	  });
 }
 }
