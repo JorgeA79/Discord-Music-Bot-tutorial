fa2a59f631731e0cd1de3a8ed0059048ae12c8dc
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-04 11:04:06 -0500

    Update marry.js

--- commands/marry.js
+++ commands/marry.js
@@ -7,17 +7,17 @@ module.exports = {
  
 	  
  message.channel.send(`Do you want to marry with ${message.author.username}`)
-	  const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+	  const collector = new Discord.MessageCollector(message.channel, m => m.author.id === "528854186176282634", { time: 10000 });
           console.log(collector)
           collector.on('collect', m => {
           if (m.content == "See") {
-		  if(m.author.id == "528854186176282634"){  
+	
                 m.channel.send("You Want To See Someones Spec OK!");
-	  }
+	  
           } else if (m.content == "Change") {
-		   if(m.author.id == "528854186176282634"){  
+	
                 m.channel.send("You Want To Change Your Spec OK!");
-          }}
+          }
     
 	  })
 }
