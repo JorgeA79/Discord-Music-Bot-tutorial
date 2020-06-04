52ef977e755c9f253c3f11026ac045a6ea55fed8
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-03 23:56:04 -0500

    Update marry.js

--- commands/marry.js
+++ commands/marry.js
@@ -4,27 +4,21 @@ module.exports = {
   name: "marry",
   description: "Pinging the bot",
   execute(client, message) {
-    
-    
+ 
+  
+const filter = (m, user) => {
+return m === message.content.includes('yes') && user.id === message.author.id;
+};  
+	  
  message.channel.send(`Do you want to marry with ${message.author.username}`).then(() => {
-	message.channel.awaitMessages(filter, { max: 999, time: 30000, errors: ['time'] })
-		.then(collected => {
-    
-    if (message.author.id === "528854186176282634"){
-      if (message.content.startsWith('Yes')) {
-        
+	message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+		        .then(collected => {
+  
 			message.channel.send(`${collected.first().author} accepted!`);
-        
-      }else if(message.content.startsWith('No')){
-            	message.channel.send(`${collected.first().author} denied!`);   
-               }else{
-               	message.channel.send(`${collected.first().author}thats not a valid answer!`);
-               }
-    }else return;
-    
-    
-		})
-		.catch(collected => {
+		
+			})
+	 
+			.catch(collected => {
 			message.channel.send('Looks like nobody got the answer this time.');
 		});
 });
