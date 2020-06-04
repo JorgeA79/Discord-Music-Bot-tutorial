7a4767dc76a30d2d1f66720897cff0bbdb73f47f
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-04 11:53:10 -0500

    Update reset.js

--- commands/reset.js
+++ commands/reset.js
@@ -1,13 +1,21 @@
 const Discord = require("discord.js");
+const pg = require('pg')
+const pool = new pg.Pool({
+	connectionString : process.env.DATABASE_URL,
+});
+
+
+pool.connect()
+
 
 module.exports = {
   name: "reset",
   description: "Pinging the bot",
   execute(client, message) {
-    const embed = new Discord.MessageEmbed()
-    .setDescription(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``)
-    .setColor(0xC76CF5);
-  message.channel.send(embed);
+
+    
+      pool.query(`UPDATE usersxp SET marry = null WHERE id = '${message.author.id}'`, console.log);		  
+
     
 }
 }
