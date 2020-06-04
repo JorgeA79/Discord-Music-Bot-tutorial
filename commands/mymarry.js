59f9eccfe0bd355a345e0baeae4c69f4cae5284e
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-04 15:11:57 -0500

    Update mymarry.js

--- commands/mymarry.js
+++ commands/mymarry.js
@@ -12,11 +12,27 @@ module.exports = {
   description: "Pinging the bot",
   execute(client, message) {
  
+	    let target = message.mentions.users.first() || message.author;
 
-	  pool.query(`SELECT * FROM usersxp WHERE id = '${message.author.id}'`, (err,resultx) =>{
-          let marryxd = resultx.rows[0].marry;
-		      let author = message.author;
-	        message.channel.send(`${author} is maried with <@${marryxd}>`)
+	  pool.query(`SELECT * FROM usersxp WHERE id = '${target.id}'`,(err, result)=>{
+          let marryxd = result.rows[0].marry;
+		
+		  
+		   if(err) return err;
+    	if(result.rows[0].marry === null){  
+    	  const embed = new Discord.MessageEmbed()
+          .setDescription(`**${target.username}** is not maried <a:x_:713677703756251147>`)
+          .setColor(0xC76CF5);
+          return message.channel.send(embed);
+	       
+    }  
+    	let marry = result.rows[0].marry;
+         const embed = new Discord.MessageEmbed()
+          .setDescription(`${message.author} is maried with <@${marry}> :sparkling_heart:`)
+          .setColor(0xC76CF5);
+         message.channel.send(embed);
+		  
+		  
 	    });
 }
 }
