b4f72ce91c18ea3776de29ef8aadfa094c84f6e9
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-04 16:53:20 -0500

    Update reset.js

--- commands/reset.js
+++ commands/reset.js
@@ -9,13 +9,28 @@ pool.connect()
 
 
 module.exports = {
-  name: "reset",
+  name: "divorce",
   description: "Pinging the bot",
   execute(client, message) {
 
     
       pool.query(`UPDATE usersxp SET marry = null WHERE id = '${message.author.id}'`, console.log);		  
-
+       pool.query(`SELECT * FROM usersxp WHERE id = '${message.author.id}'`,(err, result)=>{
+    	if(err) return err;
+    	if(!result.rows[0])  {
+	  const embed = new Discord.MessageEmbed()
+          .setDescription(`You are not married with anybody! <a:x_:713677703756251147>`)
+          .setColor(0xC76CF5);
+          message.channel.send(embed);
+	}
+	 let marry = result.rows[0].marry;       
+      	  const embed = new Discord.MessageEmbed()
+          .setDescription(`${message.author} divorced from @<${marry}>`)
+          .setColor(0xC76CF5);
+          message.channel.send(embed);
+      
+       pool.query(`UPDATE usersxp SET marry = null WHERE id = '${marry}'`, console.log);
+    }); 
     
 }
 }
