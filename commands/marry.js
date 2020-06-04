87834d7cc0a41c51bb80a0d840abc7b86ff4204a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-04 11:10:38 -0500

    Update marry.js

--- commands/marry.js
+++ commands/marry.js
@@ -5,18 +5,21 @@ module.exports = {
   description: "Pinging the bot",
   execute(client, message) {
  
+	   let member = message.mentions.members.first();
+	   if(!member) return message.reply("Try mentioning the person");
+	   if(member.user.id == message.author.id) return message.reply("You cannot marry with yourself");
 	  
- message.channel.send(`Do you want to marry with ${message.author.username}`)
-	  const collector = new Discord.MessageCollector(message.channel, m => m.author.id === "528854186176282634", { time: 10000 });
+ 	  message.channel.send(`Do you want to marry with ${member.user}`)
+	  const collector = new Discord.MessageCollector(message.channel, m => m.author.id === member.user.id, { time: 10000 });
           console.log(collector)
           collector.on('collect', m => {
-          if (m.content == "See") {
+          if (m.content == "yes") {
 	
-                m.channel.send("You Want To See Someones Spec OK!");
+                m.channel.send("Accepted");
 	  
-          } else if (m.content == "Change") {
+          } else if (m.content == "no") {
 	
-                m.channel.send("You Want To Change Your Spec OK!");
+                m.channel.send("Denied");
           }
     
 	  })
