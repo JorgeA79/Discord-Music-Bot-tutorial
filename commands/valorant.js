f0afd5795f1a5eb752731772a693a582e9e3ee02
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-05 09:03:54 -0500

    Update valorant.js

--- commands/valorant.js
+++ commands/valorant.js
@@ -11,19 +11,20 @@ module.exports = {
   description: "Pinging the bot",
   execute(client, message, args) {
     
-    
-    const title = args[1];
+for (var i=1; i<jsonContent.agents.length; i++) {
+    var agent = jsonContent.agents[i]; 
     const embed = new Discord.MessageEmbed()
     .setTitle("Valorant Agents")
-    .setDescription(`${jsonContent.emote} - **${jsonContent.name}'s** agent info`)
-    .addField("Name:", `${jsonContent.name}`, true)
-    .addField("Type:", `${jsonContent.type}`, true)
-    .addField("Description:", `${jsonContent.description}`, false)
-     .addField("Abilities:", `**[C]** ${jsonContent.abilities[0]} \n **[Q]** ${jsonContent.abilities[1] } \n **[E]** ${jsonContent.abilities[2]}\n **[X]** ${jsonContent.abilities[3]}`, false)
-    .setThumbnail(jsonContent.thumbnail)
-    .setImage(jsonContent.image)
+    .setDescription(`${agent.emote} - **${agent.name}'s** agent info`)
+    .addField("Name:", `${agent.name}`, true)
+    .addField("Type:", `${agent.type}`, true)
+    .addField("Description:", `${agent.description}`, false)
+     .addField("Abilities:", `**[C]** ${agent.abilities[0]} \n **[Q]** ${agent.abilities[1] } \n **[E]** ${agent.abilities[2]}\n **[X]** ${agent.abilities[3]}`, false)
+    .setThumbnail(agent.thumbnail)
+    .setImage(agent.image)
     .setColor(0xC76CF5);
-  message.channel.send(embed);
-    
+    message.channel.send(embed);
+      
+    }    
 }
 }
