061afe5745fcc4edde0e486e3a11dbe9b52d0782
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-05 00:14:20 -0500

    Create valorant.js

---
+++ commands/valorant.js
@@ -0,0 +1,21 @@
+//FIRST TEST HANDLER IS WORKING OR NOT
+const Discord = require("discord.js");
+const fs = require("fs");
+
+var contents = fs.readFileSync("./agents/agents.json");
+var jsonContent = JSON.parse(contents);
+
+
+module.exports = {
+  name: "valorant",
+  description: "Pinging the bot",
+  execute(client, message) {
+    const embed = new Discord.MessageEmbed()
+    .setDescription(`${jsonContent.name}, ${jsonContent.abilities}, ${jsonContent.type}`)
+    .setThumbnail(jsonContent.thubnail)
+    .setImage(jsonContent.image)
+    .setColor(0xC76CF5);
+  message.channel.send(embed);
+    
+}
+}
