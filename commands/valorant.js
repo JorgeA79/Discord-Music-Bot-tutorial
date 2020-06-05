69e2b249b97f4a13f462be21cd17de642ab3483d
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-05 07:54:49 -0500

    Update valorant.js

--- commands/valorant.js
+++ commands/valorant.js
@@ -9,9 +9,14 @@ var jsonContent = JSON.parse(contents);
 module.exports = {
   name: "valorant",
   description: "Pinging the bot",
-  execute(client, message) {
+  execute(client, message, args) {
+    
+    
+    const title = args[1];
     const embed = new Discord.MessageEmbed()
-    .setDescription(`${jsonContent.name}, ${jsonContent.abilities}, ${jsonContent.type}`)
+    .setTitle("Valorant Agents")
+    .setDescription(`${jsonContent.emote}**${title}'s** agent info`)
+    .addField("owo", `${jsonContent.name}, ${jsonContent.abilities}, ${jsonContent.type}`)
     .setThumbnail(jsonContent.thumbnail)
     .setImage(jsonContent.image)
     .setColor(0xC76CF5);
