b7514e56223cca24af392a36f74f3ef8d9b2d3ff
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-05 08:48:56 -0500

    Update valorant.js

--- commands/valorant.js
+++ commands/valorant.js
@@ -19,7 +19,7 @@ module.exports = {
     .addField("Name:", `${jsonContent.name}`, true)
     .addField("Type:", `${jsonContent.type}`, true)
     .addField("Description:", `${jsonContent.description}`, false)
-     .addField("Abilities:", `**[C]** ${jsonContent.abilities[0]} \n **[Q]** ${jsonContent.abilities[1] } \n **[E]** ${jsonContent.abilities[2]}`, false)
+     .addField("Abilities:", `**[C]** ${jsonContent.abilities[0]} \n **[Q]** ${jsonContent.abilities[1] } \n **[E]** ${jsonContent.abilities[2]}\n **[X]** ${jsonContent.abilities[3]}`, false)
     .setThumbnail(jsonContent.thumbnail)
     .setImage(jsonContent.image)
     .setColor(0xC76CF5);
