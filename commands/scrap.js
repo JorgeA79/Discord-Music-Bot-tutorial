e8e5d28c689a74616f2143bc5b40156409dec5f9
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-06 18:02:28 -0500

    Update scrap.js

--- commands/scrap.js
+++ commands/scrap.js
@@ -20,12 +20,12 @@ const cmd = args.join(" ").split(' | ');
     let embed = new Discord.MessageEmbed()
     .setTitle(`SCRAP - ${cmd}`)
     .setColor(0xC76CF5)
-    .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].URL})`, true)
-    .addField(episode.videoLinks[1].name, `[LINK 2](${episode.videoLinks[1].URL})`, true)
-    .addField(episode.videoLinks[2].name, `[LINK 3](${episode.videoLinks[2].URL})`, true)
-    .addField(episode.videoLinks[3].name, `[LINK 4](${episode.videoLinks[3].URL})`, true)
-    .addField(episode.videoLinks[4].name, `[LINK 5](${episode.videoLinks[4].URL})`, true)
-    .addField(episode.videoLinks[5].name, `[LINK 6](${episode.videoLinks[5].URL})`, true);
+    .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].url})`, true)
+    .addField(episode.videoLinks[1].name, `[LINK 2](${episode.videoLinks[1].url})`, true)
+    .addField(episode.videoLinks[2].name, `[LINK 3](${episode.videoLinks[2].url})`, true)
+    .addField(episode.videoLinks[3].name, `[LINK 4](${episode.videoLinks[3].url})`, true)
+    .addField(episode.videoLinks[4].name, `[LINK 5](${episode.videoLinks[4].url})`, true)
+    .addField(episode.videoLinks[5].name, `[LINK 6](${episode.videoLinks[5].url})`, true);
      
      message.channel.send(embed)
    })
