edca2662cfb1ae5d8b19dce90cf2151d75b128d4
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-06 17:54:24 -0500

    Update scrap.js

--- commands/scrap.js
+++ commands/scrap.js
@@ -17,6 +17,17 @@ const cmd = args.join(" ").split(' | ');
    Anime.fromName(cmd[0]).then(function (anime){
    anime.episodes[cmd[1]-1].fetch().then(function(episode){
    console.log(episode)
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`SCRAP - ${cmd}`)
+    .setColor(0xC76CF5)
+    .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].URL})`, true)
+    .addField(episode.videoLinks[1].name, `[LINK 2](${episode.videoLinks[1].URL})`, true)
+    .addField(episode.videoLinks[2].name, `[LINK 3](${episode.videoLinks[2].URL})`, true)
+    .addField(episode.videoLinks[3].name, `[LINK 4](${episode.videoLinks[3].URL})`, true)
+    .addField(episode.videoLinks[4].name, `[LINK 5](${episode.videoLinks[4].URL})`, true)
+    .addField(episode.videoLinks[5].name, `[LINK 6](${episode.videoLinks[5].URL})`, true);
+     
+     message.channel.send(embed)
    })
    })
 }
