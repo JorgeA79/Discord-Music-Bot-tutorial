8e690c23bba0a35d6bda75275c7b669462dbd129
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-06 21:09:24 -0500

    Update scrap.js

--- commands/scrap.js
+++ commands/scrap.js
@@ -32,17 +32,61 @@ const cmd = args.join(" ").split(' | ');
       var pokemonL = cmd[0].charAt(0).toUpperCase();
 			var pokemonM = cmd[0].slice(1); 
 			var pokemonX = pokemonL + pokemonM;
-     
+    
+    if(episode.videoLinks.length = 1){
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`${pokemonX}, Episode ${cmd[1]}`)
+    .setColor(0xC76CF5)
+    .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].url})`, true);
+    message.channel.send(embed)  
+    }else if(episode.videoLinks.length = 2){        
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`${pokemonX}, Episode ${cmd[1]}`)
+    .setColor(0xC76CF5)
+    .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].url})`, true)
+    .addField(episode.videoLinks[1].name, `[LINK 2](https:${episode.videoLinks[1].url})`, true);
+    message.channel.send(embed)
+    }else if(episode.videoLinks.length = 3){        
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`${pokemonX}, Episode ${cmd[1]}`)
+    .setColor(0xC76CF5)
+    .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].url})`, true)
+    .addField(episode.videoLinks[1].name, `[LINK 2](https:${episode.videoLinks[1].url})`, true)
+    .addField(episode.videoLinks[2].name, `[LINK 3](${episode.videoLinks[2].url})`, true) ;   
+    message.channel.send(embed)
+    }else if(episode.videoLinks.length = 4){        
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`${pokemonX}, Episode ${cmd[1]}`)
+    .setColor(0xC76CF5)
+    .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].url})`, true)
+    .addField(episode.videoLinks[1].name, `[LINK 2](https:${episode.videoLinks[1].url})`, true)
+    .addField(episode.videoLinks[2].name, `[LINK 3](${episode.videoLinks[2].url})`, true)  
+    .addField(episode.videoLinks[3].name, `[LINK 4](${episode.videoLinks[3].url})`, true); 	    
+    message.channel.send(embed)
+    }else if(episode.videoLinks.length = 5){        
     let embed = new Discord.MessageEmbed()
     .setTitle(`${pokemonX}, Episode ${cmd[1]}`)
     .setColor(0xC76CF5)
     .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].url})`, true)
     .addField(episode.videoLinks[1].name, `[LINK 2](https:${episode.videoLinks[1].url})`, true)
     .addField(episode.videoLinks[2].name, `[LINK 3](${episode.videoLinks[2].url})`, true)
-    .addField(episode.videoLinks[3].name, `[LINK 4](${episode.videoLinks[3].url})`, true);
-
+    .addField(episode.videoLinks[3].name, `[LINK 4](${episode.videoLinks[3].url})`, true)
+    .addField(episode.videoLinks[4].name, `[LINK 5](${episode.videoLinks[4].url})`, true);     
+    message.channel.send(embed)
+    }else if(episode.videoLinks.length = 6){        
+    let embed = new Discord.MessageEmbed()
+    .setTitle(`${pokemonX}, Episode ${cmd[1]}`)
+    .setColor(0xC76CF5)
+    .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].url})`, true)
+    .addField(episode.videoLinks[1].name, `[LINK 2](https:${episode.videoLinks[1].url})`, true)
+    .addField(episode.videoLinks[2].name, `[LINK 3](${episode.videoLinks[2].url})`, true)
+    .addField(episode.videoLinks[3].name, `[LINK 4](${episode.videoLinks[3].url})`, true)
+    .addField(episode.videoLinks[4].name, `[LINK 5](${episode.videoLinks[4].url})`, true)
+    .addField(episode.videoLinks[5].name, `[LINK 6](${episode.videoLinks[5].url})`, true); 
+    message.channel.send(embed)
+    }
      
-     message.channel.send(embed)
+     
    })
    })
     }
