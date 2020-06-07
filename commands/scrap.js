ce5cdec364c4b2b5169b8b29750d89a7928e29d2
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-06 20:41:27 -0500

    Update scrap.js

--- commands/scrap.js
+++ commands/scrap.js
@@ -39,9 +39,8 @@ const cmd = args.join(" ").split(' | ');
     .addField(episode.videoLinks[0].name, `[LINK 1](${episode.videoLinks[0].url})`, true)
     .addField(episode.videoLinks[1].name, `[LINK 2](https:${episode.videoLinks[1].url})`, true)
     .addField(episode.videoLinks[2].name, `[LINK 3](${episode.videoLinks[2].url})`, true)
-    .addField(episode.videoLinks[3].name, `[LINK 4](${episode.videoLinks[3].url})`, true)
-    .addField(episode.videoLinks[4].name, `[LINK 5](${episode.videoLinks[4].url})`, true)
-    .addField(episode.videoLinks[5].name, `[LINK 6](${episode.videoLinks[5].url})`, true);
+    .addField(episode.videoLinks[3].name, `[LINK 4](${episode.videoLinks[3].url})`, true);
+
      
      message.channel.send(embed)
    })
