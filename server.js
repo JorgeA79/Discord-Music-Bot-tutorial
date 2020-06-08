daad0362eb7490b126c4cf833ff453577ed0b360
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-08 01:29:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -285,7 +285,7 @@ client.on('message', async message => {
             		.setDescription(`Congratulations **${message.author.username}** you leveled up to level **${nextLvl}**\n\u200b`)
 			.addField("**Rewards:**", "+ $250",true) 
 	    		.setColor(0xC76CF5)
-            		.setThumbnail(message.author.avatarURL);
+            		.setThumbnail(message.author.displayAvatarURL);
 			message.channel.send(embed)	
     		}	
 			
