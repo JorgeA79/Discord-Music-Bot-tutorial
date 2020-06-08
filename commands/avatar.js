9d0c584010fec5155fb60262247996648ce0bd04
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-08 11:46:21 -0500

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: "level",
+  description: "Pinging the bot",
+  execute(client, message) {
+
+
+    let target = message.mentions.users.first() || message.author; 
+      let image = ""
+          const embed = new Discord.MessageEmbed()
+            			  .setTitle(`**${target.username}**`)
+                    .setDescription(`[Avatar URL](target.displayAvatarURL({ format: 'jpg' }))`)
+	    		          .setColor(0xC76CF5)
+            		    .setImage(target.displayAvatarURL({ format: 'jpg' }));
+      
+    message.channel.send(embed);  
+
+  
+    
+
+}
+}
