b9b1081e42d23e111df6f985ea5fc0a1a53cbb2d
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-07 23:32:39 -0500

    Update lvlup.js

--- commands/lvlup.js
+++ commands/lvlup.js
@@ -1,5 +1,12 @@
 
 const Discord = require("discord.js");
+const pg = require('pg');
+
+const pool = new pg.Pool({
+connectionString : process.env.DATABASE_URL,
+});
+
+pool.connect()
 
 module.exports = {
   name: "lvlupo",
