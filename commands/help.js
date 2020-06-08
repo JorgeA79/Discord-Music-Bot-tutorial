46b6f7a7bb1b493c2e7ebc7428e3039684452096
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-08 07:15:30 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
   .setDescription("Here you can find the main commands of the bot so you wont get confused <a:__:713677852289269761> \n\u200b")
 .setThumbnail(process.env.BOT_AVATAR)
 
-  .addField("<:discord_bot_dev:719520819771998289> Core", "\`help\` \`ping\` \`server\` \`balance\` \`daily\` \`profile\` \`marry\` \`mymarry\` \`divorce\`\n\u200b", true)
+  .addField("<:discord_bot_dev:719520819771998289> Core", "\`help\` \`ping\` \`server\` \`invite\`\n\u200b", true)
   .addField(":gem: Social", "\`profile\` \`marry\` \`mymarry\` \`divorce\`\n\u200b", true)
   .addField(":credit_card:  Economy", "\`balance\` \`daily\` \`slots\`\n\u200b", true)      
   .addField(":8ball: Fun & Games", "\`8ball\` \`flip\` \`head\` or \`tail\` \`say\` \`ship\`\n\u200b", true)   
