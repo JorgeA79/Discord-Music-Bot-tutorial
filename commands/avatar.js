3d0fe7cfdd0d039657e6521596186600771b8eb8
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-08 11:54:32 -0500

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -10,9 +10,9 @@ module.exports = {
       let image = ""
           const embed = new Discord.MessageEmbed()
             			  .setTitle(`**${target.username}**`)
-                    .setDescription(`[Avatar URL](target.displayAvatarURL({ format: 'jpg' }))`)
+                    		  .setURL(target.displayAvatarURL({ format: 'jpg' }))
 	    		          .setColor(0xC76CF5)
-            		    .setImage(target.displayAvatarURL({ format: 'jpg' }));
+            		         .setImage(target.displayAvatarURL({ format: 'jpg' }));
       
     message.channel.send(embed);  
 
