b007dc7934ba3b4a985b831566e736b68b6b7765
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-08 07:10:10 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,11 +14,12 @@ module.exports = {
   .addField("<:discord_bot_dev:719520819771998289> Core", "\`help\` \`ping\` \`server\` \`balance\` \`daily\` \`profile\` \`marry\` \`mymarry\` \`divorce\`\n\u200b", true)
   .addField(":gem: Social", "\`profile\` \`marry\` \`mymarry\` \`divorce\`\n\u200b", true)
   .addField(":credit_card:  Economy", "\`balance\` \`daily\` \`slots\`\n\u200b", true)      
-  .addField(":8ball: Fun Commands", "\`8ball\` \`flip\` \`head\` or \`tail\` \`say\` \`ship\`\n\u200b", true)   
-  .addField("<:kannaheart:712714267056537641> Roleplay Commands", "\`hug\` \`slap\` \`kiss\` \`shoot\`,\`protect\` \`wave\` \`date\` \`dance\` \`pat\`\n\u200b", true)
-  .addField(":musical_note: Music Commands", "\`play\` \`stop\` \`np\` \`queue\` \`pause\` \`resume\` \`lyrics\` \`skip\` \`clear\`\n\u200b", true)
-  .addField("<:pokeb:716936621265518613> Anime Commands", "\`pokedex\` \`anime\` \`scrap\`\n\u200b",true)
-  .addField(":video_game: Game Commands", "\`osu\` \`valorant\`\n\u200b",true) 
+  .addField(":8ball: Fun & Games", "\`8ball\` \`flip\` \`head\` or \`tail\` \`say\` \`ship\`\n\u200b", true)   
+  .addField("<:kannaheart:712714267056537641> Roleplay", "\`hug\` \`slap\` \`kiss\` \`shoot\`,\`protect\` \`wave\` \`date\` \`dance\` \`pat\`\n\u200b", true)
+  .addField(":musical_note: Music", "\`play\` \`stop\` \`np\` \`queue\` \`pause\` \`resume\` \`lyrics\` \`skip\` \`clear\`\n\u200b", true)
+  .addField("<:pokeb:716936621265518613> Anime", "\`pokedex\` \`anime\` \`scrap\`\n\u200b",true)
+  .addField(":video_game: Gaming", "\`osu\` \`valorant\`\n\u200b",true) 
+  .addField("\u200b", "\u200b",true)     
     .setTimestamp()  
   	.setFooter('Have a nice day!', process.env.BOT_AVATAR);      
   message.channel.send(embed);
