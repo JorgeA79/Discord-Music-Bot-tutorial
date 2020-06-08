f27d87c3ff4a8a4178c74f01605f836cd819186d
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-08 07:37:37 -0500

    Update ratewaifu.js

--- commands/ratewaifu.js
+++ commands/ratewaifu.js
@@ -5,7 +5,7 @@ module.exports = {
   name: "ratewaifu",
   description: "Stop the music",
   execute(client, message, args) {
-	var percentage = Math.floor(Math.random() * 10) + 1; 
+	var percentage = Math.floor(Math.random() * 10); 
 	 
    if(!args.length) {
       const embed = new Discord.MessageEmbed()
@@ -16,7 +16,7 @@ module.exports = {
  
   let embed = new MessageEmbed()
         .setColor(0xC76CF5)
-        .setDescription(`I'd give ${args.join(" ")} a ${percentage}/10 <a:x_:713677703756251147>`);    
+        .setDescription(`I'd give **${args.join(" ")}** a ${percentage}/10 <a:kawaii:713667075838705698>`);    
         message.channel.send(embed)
 	
 
