e307bfd2817efa9664989a98e7b344df42bf5552
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-09 18:53:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -340,9 +340,9 @@ client.on('message', async message => {
 	ctx.fillStyle = '#ffffff';
 	ctx.fillText(`XP: ${xp}`, canvas.width / 1.7, canvas.height / 1.8);
 	ctx.fillText(`Balance: $${money}`, canvas.width / 1.7, canvas.height / 1.25);	
-
-	ctx.font = applyText(canvas, `${normalizeText.normalizeText(`${target.username}`).toString()}!`);
-	console.log(normalizeText.normalizeText(target.username));
+	var name = normalizeText.normalizeText(target.username)
+	ctx.font = applyText(canvas, `${name}!`);
+	console.log(name);
 	ctx.fillStyle = '#ffffff';
 	ctx.fillText(`${target.username.normalize("NFC")}!`, canvas.width / 2.5, canvas.height / 3.5);
 
