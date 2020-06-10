991d7a01c9bd9feefc25451908c6357463946683
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-10 03:21:20 -0500

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -54,8 +54,8 @@ module.exports = {
 	ctx.fillStyle = '#ffffff';
 	//ctx.fillText(`XP: ${xp}`, canvas.width / 1.7, canvas.height / 1.8);
 	if(money > 1000) money = eval(money) / eval(1000)	
-	ctx.fillText(`${money}`, canvas.width / 1.7, canvas.height / 1.25);	
-	ctx.fillText(`${~~lvl}`, 245,425);	
+	ctx.fillText(`${~~money}`, canvas.width / 1.7, canvas.height / 1.25);	
+	ctx.fillText(`${lvl}`, 245,425);	
 	var name = normalizeText.normalizeText(target.username)
 	ctx.font = applyText(canvas, `${name}!`);
 	ctx.drawImage(rect, 125, 240, 250, 80);	
