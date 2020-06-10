978e54b43547d1426074e748a705c70bf2fe1f76
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-10 00:36:42 -0500

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -62,7 +62,7 @@ module.exports = {
 	ctx.clip();
 
 	const avatar = await Canvasx.loadImage(target.displayAvatarURL({ format: 'jpg' }));
-	ctx.drawImage(avatar, 25, 25, 200, 200);
+	ctx.drawImage(avatar, 170, 110, 140, 140);
 	ctx.shadowColor = '#898';
  	ctx.shadowBlur = 20;
  	ctx.shadowOffsetX = 20;
