385e7ec4e90899dc4a5263d746fd999f6f9338ea
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-11 01:34:23 -0500

    Update mine.js

--- commands/mine.js
+++ commands/mine.js
@@ -1,5 +1,12 @@
 //FIRST TEST HANDLER IS WORKING OR NOT
 const Discord = require("discord.js");
+//Wooden animation
+var anim1 = "<:woodenPix2:720518856480784435><:stone1:720518856350892119>";
+var anim2 = "<:woodenPix1:720518856694824960><:stone2:720518856694693889>";
+var anim3 = "<:woodenPix2:720518856480784435><:stone3:720518856707407942>";
+var anim4 = "<:woodenPix1:720518856694824960><:coal:720523944548892732>";
+
+
 
 module.exports = {
   name: "mine",
@@ -8,19 +15,33 @@ module.exports = {
   
   if(!args[0]){
   //Minar
-  
-  message.channel.send("<:woodenPix1:720518856694824960><:stone:720518856635973712>").then((sentMessage) => {
+  const embedU = new Discord.MessageEmbed()
+  .setDescription("<:woodenPix1:720518856694824960><:stone:720518856635973712>")
+  .setColor(0xC76CF5);
+  message.channel.send(embedU).then((sentMessage) => {
     setTimeout(function(){ 
-    sentMessage.edit("<:woodenPix2:720518856480784435><:stone1:720518856350892119>") 
+      const embed = new Discord.MessageEmbed()
+      .setDescription(`${anim1}`)
+      .setColor(0xC76CF5);  
+    sentMessage.edit(embed) 
     }, 1000);
     setTimeout(function(){ 
-    sentMessage.edit("<:woodenPix1:720518856694824960><:stone2:720518856694693889>")
+      const embed = new Discord.MessageEmbed()
+      .setDescription(`${anim2}`)
+      .setColor(0xC76CF5);  
+      sentMessage.edit(embed)
     }, 2000);
     setTimeout(function(){ 
-    sentMessage.edit("<:woodenPix2:720518856480784435><:stone3:720518856707407942>")
+      const embed = new Discord.MessageEmbed()
+      .setDescription(`${anim3}`)
+      .setColor(0xC76CF5);  
+    sentMessage.edit(embed)
     }, 3000);
    setTimeout(function(){ 
-    sentMessage.edit("<:woodenPix1:720518856694824960><:coal:720523944548892732>")
+     const embed = new Discord.MessageEmbed()
+      .setDescription(`${anim4}`)
+      .setColor(0xC76CF5);  
+    sentMessage.edit(embed)
     }, 4000);
   })
   
