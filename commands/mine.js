5224179a07e725288a8ae37ff5c644ab2fd593b4
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-11 17:54:07 -0500

    Update mine.js

--- commands/mine.js
+++ commands/mine.js
@@ -17,30 +17,100 @@ module.exports = {
   if(!args[0]){
   //Minar
   var mineralsx = [":coal:720523944548892732",":iron_ingot:720528333879771197", ":gold_ingot:720528333879640134",":diamond:720528333732839477","<:emerald:720528333862993991>"];  
-  
+      
+    
+      let testValues = [{
+      value : 'Coal',
+      probability: 0.5
+      },
+      {
+      value : 'Iron',
+      probability: 0.2
+      },
+      {
+      value : 'Gold Ingot',
+      probability: 0.1
+      },
+      {
+      value : 'Diamond',
+      probability: 0.1
+      }]
     
   if(pickaxe == "0"){
   pickaxe1 = "<:woodenPix1:720518856694824960>";
   pickaxe2 = "<:woodenPix2:720518856480784435>";
-  mineralsx = [":coal:720523944548892732",":coal:720523944548892732"];    
+  mineralsx = [":coal:720523944548892732",":coal:720523944548892732"]; 
+    
+      testValues = [{
+      value : 'Coal',
+      probability: 0.5
+      },
+      {
+      value : 'Coal',
+      probability: 0.5
+      }]  
+    
   } 
   
   if(pickaxe == "1"){
   pickaxe1 = "<:StoneP2:720617209851805752>";
   pickaxe2 = "<:stoneP:720616972944932865>"; 
-  mineralsx = [":coal:720523944548892732",":iron_ingot:720528333879771197"];    
+  mineralsx = [":coal:720523944548892732",":iron_ingot:720528333879771197"];
+    
+    testValues = [{
+      value : 'Coal',
+      probability: 0.7
+      },
+      {
+      value : 'Iron',
+      probability: 0.3
+      }]
   } 
     
   if(pickaxe == "2"){
   pickaxe1 = "<:ironP2:720617209797279777>";
   pickaxe2 = "<:ironP:720616972995526676>";
-  mineralsx = [":coal:720523944548892732",":iron_ingot:720528333879771197", ":gold_ingot:720528333879640134",":diamond:720528333732839477","<:emerald:720528333862993991>"];    
+  mineralsx = [":coal:720523944548892732",":iron_ingot:720528333879771197", ":gold_ingot:720528333879640134",":diamond:720528333732839477","<:emerald:720528333862993991>"]; 
+    
+    testValues = [{
+      value : 'Coal',
+      probability: 0.5
+      },
+      {
+      value : 'Iron',
+      probability: 0.2
+      },
+      {
+      value : 'Gold Ingot',
+      probability: 0.1
+      },
+      {
+      value : 'Diamond',
+      probability: 0.1
+      }]
   }  
     
   if(pickaxe == "3"){
   pickaxe1 = "<:pickaxeD2:720589176214519848>";
   pickaxe2 = "<:pickaxeD:720589176210325514>";
-  mineralsx = [":coal:720523944548892732",":iron_ingot:720528333879771197", ":gold_ingot:720528333879640134",":diamond:720528333732839477","<:emerald:720528333862993991>"];    
+  mineralsx = [":coal:720523944548892732",":iron_ingot:720528333879771197", ":gold_ingot:720528333879640134",":diamond:720528333732839477","<:emerald:720528333862993991>"];  
+    
+    testValues = [{
+      value : 'Coal',
+      probability: 0.5
+      },
+      {
+      value : 'Iron',
+      probability: 0.2
+      },
+      {
+      value : 'Gold Ingot',
+      probability: 0.1
+      },
+      {
+      value : 'Diamond',
+      probability: 0.1
+      }]
   }   
    
   var anim1 = `<:steve:720530349121208357>${pickaxe2}<:stone1:720518856350892119>`;
@@ -94,18 +164,7 @@ module.exports = {
    setTimeout(function(){ 
     
        //Get mineral
-      let testValues = [{
-      value : 'Coal',
-      probability: 0.1
-      },
-      {
-      value : 'Iron',
-      probability: 0.5
-      },
-      {
-      value : 'Diamond',
-      probability: 0.4
-      }]
+
 
 
       const randomizer = (values) => {
@@ -133,7 +192,7 @@ module.exports = {
        return pickedValue;
        }    
      
-       console.log(randomizer(testValues));
+       
      
        let counter = (Math.floor(Math.random() * mineralsx.length))
        var text =""; 
@@ -142,10 +201,15 @@ module.exports = {
        if(mineralsx[counter] == mineralsx[2]) text = "Gold";
        if(mineralsx[counter] == mineralsx[3]) text = "Diamond"; 
      
+     
+     
+     
+     
+       var textx = randomizer(testValues);
       const embed = new Discord.MessageEmbed()
-      .setTitle(`<:lucky:720574567571128341> | You got ${text}\n\u200b`)
+      .setTitle(`<:lucky:720574567571128341> | You got ${textx}\n\u200b`)
       .setDescription(`${anim4}<${mineralsx[counter]}>\n\u200b`)
-      .setFooter(`${message.author.username} mined some ${text}`)
+      .setFooter(`${message.author.username} mined some ${textx}`)
       .setColor(0xC76CF5);  
     sentMessage.edit(embed)
     }, 4000);
