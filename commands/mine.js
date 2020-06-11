7e929bf14800c4a922813ed58c04753ec71c28fb
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-11 17:13:28 -0500

    Update mine.js

--- commands/mine.js
+++ commands/mine.js
@@ -16,10 +16,8 @@ module.exports = {
   
   if(!args[0]){
   //Minar
-  
-  
   var mineralsx = [":coal:720523944548892732",":iron_ingot:720528333879771197", ":gold_ingot:720528333879640134",":diamond:720528333732839477"];  
-    
+  
     
   if(pickaxe == "0"){
   pickaxe1 = "<:woodenPix1:720518856694824960>";
@@ -40,17 +38,11 @@ module.exports = {
   pickaxe1 = "<:pickaxeD2:720589176214519848>";
   pickaxe2 = "<:pickaxeD:720589176210325514>";
   }   
-  
-    
-  var selecthugGif = mineralsx[Math.floor(Math.random() * mineralsx.length)];
-    
-    
+   
   var anim1 = `<:steve:720530349121208357>${pickaxe2}<:stone1:720518856350892119>`;
   var anim2 = `<:steve:720530349121208357>${pickaxe1}<:stone2:720518856694693889>`;
   var anim3 = `<:steve:720530349121208357>${pickaxe2}<:stone3:720518856707407942>`;
   var anim4 = `<:steve:720530349121208357>${pickaxe1}`;
-    
-
   var text ="";  
     
   if(selecthugGif = mineralsx[0]) text = "Coal";
@@ -90,9 +82,10 @@ module.exports = {
     sentMessage.edit(embed)
     }, 3000);
    setTimeout(function(){ 
+     let counter = (Math.floor(Math.random() * mineralsx.length))
      const embed = new Discord.MessageEmbed()
       .setTitle(`<:lucky:720574567571128341> | You got ${text}\n\u200b`)
-      .setDescription(`${anim4}<${mineralsx[2]}>\n\u200b`)
+      .setDescription(`${anim4}<${mineralsx[counter]}>\n\u200b`)
       .setFooter(`${message.author.username} mined some ${text}`)
       .setColor(0xC76CF5);  
     sentMessage.edit(embed)
