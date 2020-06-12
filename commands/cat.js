3f822202a1e6c4c597b795bf04be3b9eac7ff83b
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-12 05:23:03 -0500

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,24 +1,24 @@
 //FIRST TEST HANDLER IS WORKING OR NOT
 const Discord = require("discord.js");
 const superagent = require("superagent")
-
+const fetch = require('node-fetch');
 
 module.exports = {
   name: "cat",
   description: "Pinging the bot",
   execute(client, message) {
    
-   
-   let {body} = superagent.get('http://aws.random.cat/meow')
-    
-    if(!{body}) return message.channel.send("I broke! Try again.")
-    
-    const embed = new Discord.MessageEmbed()
-    .setDescription(`:cat: | Here is your image!`)
-    .setColor(0xC76CF5)
-    .setImage(body.file)
-    .setFooter('Have a nice day!', process.env.BOT_AVATAR)
-    message.channel.send(embed);
+    fetch("http://aws.random.cat/meow")
+        .then(res => res.json()).then(body => {
+            if(!body) return message.reply(" whoops. I broke, try again!")
+
+              const embed = new Discord.MessageEmbed()
+              .setDescription(`:cat: | Here is your image!`)
+              .setColor(0xC76CF5)
+              .setImage(body.file)
+              .setFooter('Have a nice day!', process.env.BOT_AVATAR)
+              message.channel.send(embed);
+        })  
     
 }
 }
