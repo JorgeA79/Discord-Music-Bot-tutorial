69343cab3692cb08ed9e5c939dd2de2bdb75629a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-12 20:17:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -366,4 +366,49 @@ client.on('message', async message => {
 });
 
 
+client.on('message',async message => {
+	  if (message.author === client.user) return;
+	  if (message.content.startsWith(PREFIX + "apex")) {
+		
+		const args = message.content.slice(PREFIX.length).split(` `);
+		
+		if(!args[0]) return message.channel.send("Please supply a username.");
+        	if(!args[1]) return message.channel.send("Please supply a platform to check. `pc`, `xbox` or `ps4`");
+
+        	const platformCheck = { pc: API.Platform.PC, xbox: API.Platform.XBOX_ONE, ps4: API.Platform.PS4 };
+        	const platform = platformCheck[args[1].toLowerCase()];
+			
+		try {
+                const results = await ApexTab.searchPlayer(args[0], platform ? platform : API.Platform.PC)
+            
+                for (let playerResult of results.results) {
+                    const player = await ApexTab.getPlayerById(playerResult.aid)
+                    const { name, skillratio, visits, avatar, legend, level, kills, headshots, matches, globalrank, utime } = player;
+
+                     const embed = new discord.MessageEmbed()
+                            .setColor(0xC76CF5)
+                            .setAuthor(`Origin (Apex Legends) | ${name}`, avatar)
+                            .setThumbnail(avatar)
+                            .setDescription(stripIndents`
+                                **Active Legend:** ${legend || "Not Found."}
+                                **Global Rank:** ${globalrank || "Not Ranked."}
+                                **level:** ${level || 0}
+                                **Skill Ratio:** ${skillratio || "0%"}
+                                **Matches:** ${matches || 0}
+                                **Kills:** ${kills || 0}
+                                **Headshots:** ${headshots || 0}
+                                **Visits:** ${visits || 0}
+                                **PlayTime:** ${Math.ceil(utime / (1000 * 60 * 60 * 24)) || 0} days
+                            `)
+                            .setTimestamp()
+			   message.channel.send(embed)
+                 	}
+        		}catch(err) {
+            		return message.channel.send("Can't find a player by that")
+        		}
+		
+	}
+});
+
+
 client.login(process.env.BOT_TOKEN)
