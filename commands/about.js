a72bb87a4ef13b5a3362e262553e81d21df3fab6
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-13 03:32:39 -0500

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -9,9 +9,9 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     .setTitle(`Pixel Bot`)
     .setDescription(`xd`)
-    .addField("\u200b", "<:Pixel:721279438406418503> PixelEdits#8890", true)
-    .addField("Helpers & Staff:", "<:Rii:721279439056666635> RiiDaPeanut#4066", true)
-    .addField("\u200b", "<:Ammy:721279438192377895> AmmyZeru#4409", true)
+    .addField("\u200b", "<:Pixel:721279438406418503> PixelEdits", true)
+    .addField("Helpers & Staff:", "<:Rii:721279439056666635> RiiDaPeanut", true)
+    .addField("\u200b", "<:Ammy:721279438192377895> AmmyZeru", true)
     .setColor(0xC76CF5);
     message.channel.send(embed);
     
