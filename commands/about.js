68531f3db3f6088cb76af293022904591630dd8a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-13 04:09:20 -0500

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -8,10 +8,12 @@ module.exports = {
   
     const embed = new Discord.MessageEmbed()
     .setTitle(`Pixel Bot`)
+    .setThumbnail(process.env.BOT_AVATAR)
+    .setImage("https://media.discordapp.net/attachments/396942894487044099/719703607963942994/unknown.png")
     .setDescription(`PixelBot is a bot with several functions and games for having some fun and spending time on discord, we hope you to like our bot!`)
     .addField("Functions:", "<:discord_bot_dev:719520819771998289> Core\n:gem: Social\n:credit_card:  Economy\n", true)
     .addField("\u200b", ":8ball: Fun & Games\n<:kannaheart:712714267056537641> Roleplay\n:musical_note: Music", true)
-    .addField("\u200b", "<:pokeb:716936621265518613> Anime\n:video_game: Gaming\n⚙️ Utility", true)
+    .addField("\u200b", "<:pokeb:716936621265518613> Anime\n:video_game: Gaming\n⚙️ Utility\n\u200b", true)
     .addField("\u200b", "\u200b", true)
     .addField("Main Coder", "<:FinalWords:721283729989042284> 𝒥𝑜𝓇𝑔𝓎\n\u200b", true)
     .addField("\u200b", "\u200b", true)
