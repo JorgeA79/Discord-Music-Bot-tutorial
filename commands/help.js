1e138026c418b32e8f995fe5a6add2da6badb25a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-16 02:57:05 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,12 +5,11 @@ module.exports = {
   description: "Help",
   execute(client, message, args) {
   
-     	const embed = new Discord.MessageEmbed()
-  .setTitle("Pixel's Commands")
+   const embed = new Discord.MessageEmbed()
+   .setTitle("Pixel's Commands")
    .setColor(0xC76CF5)
-  .setDescription("Here you can find the main commands of the bot so you wont get confused <a:__:713677852289269761> \n\u200b")
-.setThumbnail(process.env.BOT_AVATAR)
-
+   .setDescription("Here you can find the main commands of the bot so you wont get confused <a:__:713677852289269761> \n\u200b")
+   .setThumbnail(process.env.BOT_AVATAR)
   .addField("<:discord_bot_dev:719520819771998289> Core", "\`help\` \`ping\` \`server\` \`invite\` \`support\` \`serverinfo\`\n\u200b", true)
   .addField(":gem: Social", "\`profile\` \`marry\` \`mymarry\` \`divorce\` \`level\`\n\u200b", true)
   .addField(":credit_card:  Economy", "\`balance\` \`daily\` \`slots\` \`catch sell\` \`mine sell\`\n\u200b", true)      
@@ -20,8 +19,21 @@ module.exports = {
   .addField("<:pokeb:716936621265518613> Anime", "\`pokedex\` \`anime\` \`scrap\`\n\u200b",true)
   .addField(":video_game: Gaming", "\`osu\` \`valorant\` \`fortnite\` \`lol\`\n\u200b",true) 
   .addField("⚙️ Utility", " \`urban\` \`avatar\` \`say\` \`cat\` \`dog\` \`fox\` \`quote\`\n\u200b",true)     
-    .setTimestamp()  
-  	.setFooter('Have a nice day!', process.env.BOT_AVATAR);      
+  .setTimestamp()  
+  .setFooter('Have a nice day!', process.env.BOT_AVATAR);   
+	  
+  const embed2 = new Discord.MessageEmbed()
+   .setTitle("Pixel's Commands")
+   .setColor(0xC76CF5)
+   .setDescription("Here you can find the main commands of the bot so you wont get confused <a:__:713677852289269761> \n\u200b")
+   .setThumbnail(process.env.BOT_AVATAR)
+  .addField("<:kannaheart:712714267056537641> Roleplay", "\`hug\` \`slap\` \`kiss\` \`shoot\` \`protect\` \`wave\` \`date\` \`dance\` \`pat\`\n\u200b", true)
+  .addField("<:pokeb:716936621265518613> Anime", "\`pokedex\` \`anime\` \`scrap\`\n\u200b",true)
+  .addField(":video_game: Gaming", "\`osu\` \`valorant\` \`fortnite\` \`lol\`\n\u200b",true)   
+  .setTimestamp()  
+  .setFooter('Have a nice day!', process.env.BOT_AVATAR);    	  
+	  
+	  
   message.channel.send(embed)
     .then(msg => { msg.react('1️⃣')
                    msg.react('2️⃣')
@@ -33,7 +45,9 @@ module.exports = {
 	                        .then(collected => {
 		                      const reaction = collected.first();
 		                      if (reaction.emoji.name === '1️⃣') {
-			                    
+			              msg.edit(embed)     
+		                      }else if (reaction.emoji.name === '2️⃣') {
+			              msg.edit(embed2)          
 		                      }
 	                        })
 	                        .catch(collected => {
