0897e62747439bccfb9b4eb4b3430bca6bebab08
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-18 05:23:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -500,52 +500,6 @@ client.on('message', async message => {
 	  }
 	  });
 	  
-    client.on('message', async message => {
-	  if (message.author === client.user) return;
-	  if (message.content.startsWith(PREFIX + "overwatch")) {
-		  
-	  const args = message.content.slice(PREFIX.length).split(` `);
-		  
-	 if(!args[1]) return message.channel.send("Please supply a username.");
-         if(!args[2] || (args[2] && !["pc", "xbl", "psn"].includes(args[1]))) return message.channel.send("Please supply a platform to check. `pc`, `xbox` or `psn`");
-         if(args[1].includes("#")) args[1] = args[1].replace(/#/g, "-");
-
-            overwatch.getProfile(args[1], "global", args[0], (err, json) => {
-                if (err) return message.channel.send("Unable to find a user with that username.");
-                const { games, level, portrait, username, playtime: { competitive, quickplay }, private } = json;
-                const { sportsmanship, shotcaller, teammate } = json.endorsement;
-                const { won, draw, played, lost, win_rate } = json.games.competitive;
-
-                if(private) return message.channel.send("This users stats are private and cant be seen by anyone.");
-                        
-                    const embed = new discord.MessageEmbed()
-                        .setColor(0xC76CF5)
-                        .setAuthor(`Blizzard (Overwatch) | ${username}`, portrait)
-                        .setThumbnail(portrait)
-                        .addField("General:", stripIndents`
-                        **Level:** ${level || 0}
-                        **Sportsmanship:** ${sportsmanship.rate || 0} / 100
-                        **Shotcaller:** ${shotcaller.rate || 0} / 100
-                        **Teammate:** ${teammate.rate  || 0} / 100
-                        `)
-                        .addField("Competitive:", stripIndents`
-                        **Played:** ${played || 0}
-                        **Won:** ${won || 0}
-                        **Draw:** ${draw || 0}
-                        **Lost:** ${lost || 0}
-                        **Win Rate:** ${win_rate || 0}
-                        **Playtime:** ${competitive || 0}
-                        `, true)
-                        .addField("QuickPlay:", stripIndents`
-                        **Played:** ${games.quickplay.played || "N/A"}
-                        **Won:** ${games.quickplay.won || 0}
-                        **Playtime:** ${quickplay || 0}
-                        `, true)
-                        .setTimestamp();
-
-                    message.channel.send(embed);
-		     })
-	  }
-	  });	  
+   
 
 client.login(process.env.BOT_TOKEN)
