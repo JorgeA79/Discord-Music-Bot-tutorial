7c9017f672738ebe80050a09bfed6d7ba62558ee
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-06-28 23:32:36 -0500

    Create idv.js

---
+++ commands/idv.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+var contents = fs.readFileSync("./commands/agents/agents.json");
+var jsonContent = JSON.parse(contents);
+
+
+module.exports = {
+  name: "idv",
+  description: "Pinging the bot",
+  execute(client, message, args) {
+
+  if(args[0] == "survivors"){
+  if(!args[1]){
+  return message.reply("no lul");
+  }else{
+  
+  const argsowo = args.splice(1).join(" ");  
+  const idv = {
+  "EMMA WOODS":jsonContent. survivors[0]
+  }
+  const embed = new Discord.MessageEmbed()
+    .setTitle("Identity V Survivors")
+    .setDescription(`Here's **idv[argsowo.toUpperCase()].name** Background Story and abilities!`)
+    .addField("Name:", `idv[argsowo.toUpperCase()].name`, true)
+    .setImage(idv[argsowo.toUpperCase()].image)
+    .setColor(0xC76CF5);.
+  }
+  }else{
+  return message.reply("no xd");
+  }
+
+
+
+
+    
+}
+}
