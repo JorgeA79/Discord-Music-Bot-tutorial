d55af3f31d71f017b2f6ef342c8620f6fed9caa2
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-09 15:09:21 -0500

    Create servers.js

---
+++ commands/servers.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "verify",
+  description: "Pinging the bot",
+  execute(client, message) {
+    const embed = new Discord.MessageEmbed()
+    .setDescription(`Im currently in ${client.guilds.cache.size} / 100 Servers for getting verified <a:kawaii:713667075838705698>``)
+    .setColor(0xC76CF5);
+  message.channel.send(embed);
+    
+}
+}
