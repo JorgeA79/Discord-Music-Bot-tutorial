a6a91d7259e0450da9f9393ef587a95b3e1d5f22
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-09 15:11:06 -0500

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "Pinging the bot",
   execute(client, message) {
     const embed = new Discord.MessageEmbed()
-    .setDescription(`Im currently in ${client.guilds.cache.size} / 100 Servers for getting verified <a:kawaii:713667075838705698>``)
+    .setDescription(`Im currently in ${client.guilds.cache.size} / 100 Servers for getting verified <a:kawaii:713667075838705698>`)
     .setColor(0xC76CF5);
   message.channel.send(embed);
     
